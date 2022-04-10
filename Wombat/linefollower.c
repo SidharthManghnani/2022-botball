c51cfc8814a6eaf2ea373e8082c42f1632f15a3c
AgentFox29 <98663744+AgentFox29@users.noreply.github.com>
2022-04-10 12:13:34 -0400

    linefollower.h v 1.2

--- Wombat/linefollower.c
+++ Wombat/linefollower.c
@@ -5,42 +5,40 @@ void linefollowfordistance_cm (int sensor_port, int distancein_cm)
 
 {
     float distanceTraveled;
-    while (distanceTraveled < distancein_cm)
+    if (sensor_port == lightsensorport_left);
+    
+        while (distanceTraveled < distancein_cm)
+        {
+            if (analog(sensor_port) > 1600)
+            {
+                motor(motor_right, 90);
+                motor(motor_left, 5);
+            }
+            else
+            {
+                motor(motor_right, 5);
+                motor(motor_left, 90);
+            }
+            ao();
+            
+            (get_motor_position_counter(motor_left) + get_motor_position_counter(motor_right))*cmPerTick = distanceTraveled;
+        }
+    if (sensor_port == lightport_right);
+    
+        while (distanceTraveled < distancein_cm)
     {
-        if (analog(sensor_port) > 1600)
-        {
-            motor(motor_right, 90);
-            motor(motor_left, 5);
-        }
-        else
-        {
-            motor(motor_right, 5);
-            motor(motor_left, 90);
-        }
-        ao();
-        
-        (get_motor_position_counter(motor_left) + get_motor_position_counter(motor_right))*cmPerTick = distanceTraveled;
-    }
-}
-
-void other_linefollowfordistance_cm (int sensor_port, int distancein_cm)
-
-{
-    float distanceTraveled;
-    while (distanceTraveled < distancein_cm)
-    {
-        if (analog(sensor_port) > 1600)
-        {
-            motor(motor_left, 90);
-            motor(motor_right, 5);
-        }
-        else
-        {
-            motor(motor_left, 5);
-            motor(motor_right, 90);
-        }
-        ao();
-        
-        (get_motor_position_counter(motor_left) + get_motor_position_counter(motor_right))*cmPerTick = distanceTraveled;
+            if (analog(sensor_port) > 1600)
+            {
+                motor(motor_left, 90);
+                motor(motor_right, 5);
+            }
+            else
+            {
+                motor(motor_left, 5);
+                motor(motor_right, 90);
+            }
+            ao();
+            
+            (get_motor_position_counter(motor_left) + get_motor_position_counter(motor_right))*cmPerTick = distanceTraveled;
     }
 }
--- Wombat/linefollower.h
+++ Wombat/linefollower.h
@@ -3,3 +3,5 @@ void linefollowfordistance_cm (int motor_left, int motor_right, int sensor_port,
 #define float cmPerTick = 0.011345;
 #define int motor_left = 0;
 #define int motor_right = 1;
+#define int lightport_left = 9;
+#define int lightport_right = 0;
