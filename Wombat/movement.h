fa7f4222a38e7bc630dfacf745078caae01c5388
JeffLegendPower <80235181+JeffLegendPower@users.noreply.github.com>
2022-04-10 10:12:28 -0400

    Create movement.h

---
+++ Wombat/movement.h
@@ -0,0 +1,5 @@
+void turn_left_spin(int degrees, int speed);
+void turn_right_spin(int degrees, int speed);
+
+void turn_left_pivot(int degrees, int speed);
+void turn_right_pivot(int degrees, int speed);
