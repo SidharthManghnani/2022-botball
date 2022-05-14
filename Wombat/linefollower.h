2b61c3c4245b16b184fd5944b092fa53f97caa3d
SidharthManghnani <101518562+SidharthManghnani@users.noreply.github.com>
2022-05-14 08:42:03 -0400

    Delete linefollower.h

--- Wombat/linefollower.h
+++
@@ -1,7 +0,0 @@
-void linefollowfordistance_cm (int motor_left, int motor_right, int sensor_port, int distancein_cm);
-
-#define float cmPerTick = 0.011345;
-#define int motor_left = 0;
-#define int motor_right = 1;
-#define int lightport_left = 9;
-#define int lightport_right = 0;
