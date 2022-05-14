f58cc2bc4863d10bd42dfd1dfca355b5e084d38d
SidharthManghnani <101518562+SidharthManghnani@users.noreply.github.com>
2022-05-14 08:47:37 -0400

    Create linefollowing.h

---
+++ Wombat/linefollowing.h
@@ -0,0 +1,12 @@
+void forwardUntilBlack(int sensor_port);
+void followLineMilliseconds(int millis, int speed);
+
+Movement.h
+void forward(int mm, int speed);
+void forwardForSeconds(int speed, int timeMillis);
+
+void turn_left_spin(int degrees);
+void turn_right_spin(int degrees);
+void rings();
+void turn_left_pivot(int degrees);
+void turn_right_pivot(int degrees);
